03f67a5ccc61a55dd63d5b34469428249c15073c
RobertoSHrdzReyes <118469441+RobertoSHrdzReyes@users.noreply.github.com>
2022-11-17 11:54:55 -0600

    Add files via upload

---
+++ Capitulo2/procesar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="Dectription" content="HTML,CSS,Javascript">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Respuesta</title>
+</head>
+<body>
+    <header></header>
+    <nav></nav>
+    <main>
+    	<section>
+    		<p>
+    			<?php
+    			  print('El código postal es: '.$_GET['buscar']);
+    			?>
+    		</p>
+    	</section>
+    	<aside></aside>
+    </main>
+    <footer></footer>
+</body>
+</html>
